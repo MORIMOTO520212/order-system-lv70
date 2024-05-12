3d8c0677e30627d430e3fc0972279559e946d7a5
Yuma Morimoto <kanten520212@gmail.com>
2024-05-13 00:29:02 +0900

    update: models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -1,11 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+final class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'product_id',
+        'quantity',
+        'unit_price',
+        'shipped_on',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
