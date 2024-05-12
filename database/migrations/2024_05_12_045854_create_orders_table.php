6cd9b0a1c16b9c0a745099b8f2214e257e827a4f
Yuma Morimoto <kanten520212@gmail.com>
2024-05-12 15:38:24 +0900

    update migrations

--- database/migrations/2024_05_12_045854_create_orders_table.php
+++ database/migrations/2024_05_12_045854_create_orders_table.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class() extends Migration
 {
     /**
      * Run the migrations.
@@ -21,6 +23,9 @@ return new class extends Migration
             $table->integer('unit_price')->unsigned();
             $table->date('shipped_on')->nullable();
             $table->timestamps();
+
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('product_id')->references('id')->on('products');
         });
     }
 
